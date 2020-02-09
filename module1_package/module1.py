a937eeb328f04fc5925389cc879ccd3e9dccb30c
liyang <235938019@qq.com>
2020-02-09 03:49:28 +0000

    liyang

--- module1_package/module1.py
+++
@@ -1,6 +0,0 @@
-def sum(*a):
- result=0
- for i in a:
-  result += i
- return result
-
--- module2_package/module3.py
+++
@@ -1,6 +0,0 @@
-import sys
-sys.path.append('/root/git/python_mysql')
-from module1_package.module1 import sum
-#print(sys.path)
-a=sum(1,3,5)
-print(a)
