2bb2f83cd41c73d30f4c86164a0d41c824b85c9c
liyang <235938019@qq.com>
2020-02-15 12:22:17 +0000

    liyang

--- python/s8_data_time/time.py
+++ python/s8_data_time/time.py
@@ -12,4 +12,14 @@ time.struct_time(tm_year=1970, tm_mon=1, tm_mday=1, tm_hour=0, tm_min=0, tm_sec=
 >>> time.gmtime()
 time.struct_time(tm_year=2020, tm_mon=2, tm_mday=15, tm_hour=12, tm_min=7, tm_sec=46, tm_wday=5, tm_yday=46, tm_isdst=0)
 
+>>> time.time()
+1581768997.9780233
+>>> a=time.time()
+>>> time.gmtime(a)
+time.struct_time(tm_year=2020, tm_mon=2, tm_mday=15, tm_hour=12, tm_min=16, tm_sec=46, tm_wday=5, tm_yday=46, tm_isdst=0)
 
+>>> time.localtime()
+time.struct_time(tm_year=2020, tm_mon=2, tm_mday=15, tm_hour=7, tm_min=18, tm_sec=50, tm_wday=5, tm_yday=46, tm_isdst=0)
+
+b = time.localtime()
+time.strftime("%a, %d %b %Y %H:%M:%S +0000",b)
