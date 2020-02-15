daee8f15424b02d23769dae1f0d03aa0391ee19c
liyang <235938019@qq.com>
2020-02-15 11:22:28 +0000

    liyang

---
+++ python/s7_exception/try_except_else_finally.py
@@ -0,0 +1,17 @@
+def divide(x,y):
+  try:
+    result=x/y
+    a=[1]
+    print(a[1])
+  except ZeroDivisionError:
+    print("division by zero!")
+  except TypeError:
+    print("type error")
+  except Exception:
+    print("base exception")
+  else:
+    print("result is", result)
+  finally:
+    print("executing finally clause")
+
+divide(1,0)
