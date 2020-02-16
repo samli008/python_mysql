d9d131177d36176d6778713d40e03937aaf906da
liyang <235938019@qq.com>
2020-02-16 16:33:16 +0000

    liyang

--- mysql/mysql.py
+++ mysql/mysql.py
@@ -2,12 +2,27 @@
 pip3 install pymysql -i https://mirrors.aliyun.com/pypi/simple
 import pymysql
 
-# connect mysql ip user passwd database port
+# connect mysql with ip user passwd database port
 connection=pymysql.connect("192.168.20.161","root","liyang","liyang",3306)
 cursor=connection.cursor()
+
 sql="insert into person(name,phone,age)values('xxx','18116583538',40);"
 count=cursor.execute(sql)
 print(count)
 connection.commit()
+
+cursor.close()
+connection.close()
+
+# select database
+connection=pymysql.connect("192.168.20.161","root","liyang","liyang",3306)
+cursor=connection.cursor()
+
+sql="select * from person;"
+count=cursor.execute(sql)
+print(count)
+result=cursor.fetchall()
+print(result)
+
 cursor.close()
 connection.close()
