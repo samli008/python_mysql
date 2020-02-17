cb9d8ee31f7e2dfe8df95c11958df8b93d0bbf9f
liyang <235938019@qq.com>
2020-02-17 08:40:45 +0000

    liyang

--- python/s9_debug_unit_test/debug_pdb.py
+++ python/s9_debug_unit_test/debug_pdb.py
@@ -15,7 +15,8 @@ def all_even(num_list):
 
 print(all_even(numbers))
 
-# pdb mode demo
+# pdb mode demo with list step(s)
+# step(s) into function()
 (Pdb) s
 > /root/debug_pdb.py(12)all_even()
 -> even_numbers.extend(number)
@@ -37,3 +38,19 @@ TypeError: 'int' object is not iterable
 -> even_numbers.extend(number)
 (Pdb) p number
 2
+
+# pdb mode demo with list next(n)
+# next(n) don't into function()
+(Pdb) list
+ 10       for number in num_list:
+ 11         if number%2==0:
+ 12           even_numbers.extend(number)
+ 13       return even_numbers
+ 14  
+ 15  -> print(all_even(numbers))
+ 16  
+[EOF]
+(Pdb) n
+TypeError: 'int' object is not iterable
+> /root/debug_pdb.py(15)<module>()
+-> print(all_even(numbers))
