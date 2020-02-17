c9f6b75ec673efe78380e8c965f0e7198365ad9a
liyang <235938019@qq.com>
2020-02-17 02:14:43 +0000

    liyang

--- python/s9_debug_unit_test/bug.py
+++ python/s9_debug_unit_test/bug.py
@@ -10,6 +10,15 @@ def all_even(num_list):
 
 print(all_even(numbers))
 
+"""
+Traceback (most recent call last):
+  File "bug.py", line 10, in <module>
+    print(all_even(numbers))
+  File "bug.py", line 7, in all_even
+    even_numbers.extend(number)
+TypeError: 'int' object is not iterable
+"""
+
 # demo normal
 numbers=[1,2,3,4,5,6,10,-4,-7,0]
 
