623eb2dc0b67d164ac4cf76e6b9a255dfaa9dda0
liyang <235938019@qq.com>
2020-02-18 04:29:07 +0000

    liyang

--- python/s9_debug_unit_test/main/test_math_func.py
+++ python/s9_debug_unit_test/main/test_math_func.py
@@ -1,9 +1,20 @@
 import sys
 del sys.path[0]
 sys.path.append('/root/git/python_mysql/python/s9_debug_unit_test')
-print(sys.path)
 
 import unittest
 from main.math_func import add
 
-class 
+class TestAdd(unittest.TestCase):
+  def test_add(self):
+    self.assertEqual(add(1,2),3)
+    self.assertEqual(add(10,2),12)
+
+  def test_add_again(self):
+    self.assertEqual(add(11,22),33)
+
+  def test_add_exception(self):
+    self.assertRaises(ValueError,add,'a',1)
+
+if __name__ == "__main__":
+    unittest.main() 
