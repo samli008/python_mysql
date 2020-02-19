11a3ed4e32a6740c446700f5b32f2d53e7730fa3
liyang <235938019@qq.com>
2020-02-19 17:11:27 +0000

    liyang

--- mysql/excel.py
+++ mysql/excel.py
@@ -1,6 +1,7 @@
 # pip3 install xlwt -i https://mirrors.aliyun.com/pypi/simple
 # pip3 install xlrd -i https://mirrors.aliyun.com/pypi/simple
 
+# excel write with python
 import xlwt
 workbook=xlwt.Workbook()
 liyang=workbook.add_sheet('liyang')
@@ -10,6 +11,7 @@ liyang.write(1,0,'liyang')
 liyang.write(1,1,'1929')
 workbook.save('/root/liyang.xls')
 
+# excel read with python
 import xlrd
 workbook = xlrd.open_workbook('/root/liyang.xls')
 #liyang = workbook.sheet_by_index(0)
@@ -19,6 +21,7 @@ print(liyang.cell(0,1).value )
 print(liyang.cell(1,0).value )
 print(liyang.cell(1,1).value )
 
+# excel write with list and for
 import xlwt
 workbook = xlwt.Workbook()
 liyang = workbook.add_sheet('liyang')
@@ -34,4 +37,13 @@ for i in range(0, len(data)):
 
 workbook.save('/root/sam.xls')
 
+# excel read with list and for
+import xlrd
+workbook = xlrd.open_workbook('/root/sam.xls')
+liyang=workbook.sheet_by_index(0)
 
+for i in range(0,liyang.nrows):
+  value = []
+  for j in range(0,liyang.ncols):
+    value.append(liyang.cell(i,j).value )
+  print(value)
