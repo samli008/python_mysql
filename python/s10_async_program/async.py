143556fd0cdcc199cd379f0f832cc57bc269aaa2
liyang <235938019@qq.com>
2020-02-19 08:51:39 +0000

    liyang

---
+++ python/s10_async_program/async.py
@@ -0,0 +1,52 @@
+# sync demo
+import time
+from datetime import datetime
+
+def print_message(interval_seconds,message='keep alive'):
+    while True:
+        print(f'{datetime.now()}-{message}')
+        start=time.time()
+        end=start+interval_seconds
+        while True:
+            now=time.time()
+            if now >= end:
+                break
+
+if __name__=="__main__":
+    print_message(3,'three')
+
+# async demo with yield next()
+import time
+from datetime import datetime
+
+def print_message(interval_seconds,message='keep alive'):
+    while True:
+        print(f'{datetime.now()}-{message}')
+        start=time.time()
+        end=start+interval_seconds
+        while True:
+            yield
+            now=time.time()
+            if now >= end:
+                break
+
+if __name__=="__main__":
+    a=print_message(3,'three')
+    b=print_message(10,'ten')
+    stack=[a,b]
+    while True:
+        for task in stack:
+            next(task)
+"""
+[root@c03 ~]# python sync2.py 
+2020-02-19 03:47:27.775670-three
+2020-02-19 03:47:27.775958-ten
+2020-02-19 03:47:30.775959-three
+2020-02-19 03:47:33.776087-three
+2020-02-19 03:47:36.776254-three
+2020-02-19 03:47:37.776009-ten
+2020-02-19 03:47:39.776364-three
+2020-02-19 03:47:42.776486-three
+2020-02-19 03:47:45.776608-three
+2020-02-19 03:47:47.776139-ten
+"""
