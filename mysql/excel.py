1d238d925eb6f17424e5d642916d248ae1bf7190
liyang <235938019@qq.com>
2020-02-19 19:59:25 +0000

    liyang

--- mysql/excel.py
+++ mysql/excel.py
@@ -3,28 +3,28 @@
 
 # excel write with python
 import xlwt
-workbook=xlwt.Workbook()
-liyang=workbook.add_sheet('liyang')
-liyang.write(0,0,'name')
-liyang.write(0,1,'id')
-liyang.write(1,0,'liyang')
-liyang.write(1,1,'1929')
-workbook.save('/root/liyang.xls')
+book=xlwt.Workbook()
+sheet=book.add_sheet('liyang')
+sheet.write(0,0,'name')
+sheet.write(0,1,'id')
+sheet.write(1,0,'liyang')
+sheet.write(1,1,'1929')
+book.save('/root/liyang.xls')
 
 # excel read with python
 import xlrd
-workbook = xlrd.open_workbook('/root/liyang.xls')
-#liyang = workbook.sheet_by_index(0)
-liyang = workbook.sheet_by_name('liyang')
-print(liyang.cell(0,0).value )
-print(liyang.cell(0,1).value )
-print(liyang.cell(1,0).value )
-print(liyang.cell(1,1).value )
+book = xlrd.open_workbook('/root/liyang.xls')
+#sheet = book.sheet_by_index(0)
+sheet = book.sheet_by_name('liyang')
+print(sheet.cell(0,0).value )
+print(sheet.cell(0,1).value )
+print(sheet.cell(1,0).value )
+print(sheet.cell(1,1).value )
 
 # excel write with list and for
 import xlwt
-workbook = xlwt.Workbook()
-liyang = workbook.add_sheet('liyang')
+book = xlwt.Workbook()
+sheet = book.add_sheet('liyang')
 data = [
   ['name', 'id'],
   ['liyang', '1929'],
@@ -33,17 +33,17 @@ data = [
 
 for i in range(0, len(data)):
   for j in range(0, len(data[i])):
-    liyang.write(i, j, data[i][j] )
+    sheet.write(i, j, data[i][j] )
 
-workbook.save('/root/sam.xls')
+book.save('/root/sam.xls')
 
 # excel read with list and for
 import xlrd
-workbook = xlrd.open_workbook('/root/sam.xls')
-liyang=workbook.sheet_by_index(0)
+book = xlrd.open_workbook('/root/sam.xls')
+sheet=book.sheet_by_index(0)
 
-for i in range(0,liyang.nrows):
+for i in range(0,sheet.nrows):
   value = []
-  for j in range(0,liyang.ncols):
-    value.append(liyang.cell(i,j).value )
+  for j in range(0,sheet.ncols):
+    value.append(sheet.cell(i,j).value )
   print(value)
