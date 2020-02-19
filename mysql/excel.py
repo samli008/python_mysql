e83122bcccb231c718b841d2956a488aa6aa17ed
liyang <235938019@qq.com>
2020-02-19 16:57:35 +0000

    liyang

--- mysql/excel.py
+++ mysql/excel.py
@@ -12,9 +12,26 @@ workbook.save('/root/liyang.xls')
 
 import xlrd
 workbook = xlrd.open_workbook('/root/liyang.xls')
-liyang = workbook.sheet_by_index(0)
+#liyang = workbook.sheet_by_index(0)
+liyang = workbook.sheet_by_name('liyang')
 print(liyang.cell(0,0).value )
 print(liyang.cell(0,1).value )
 print(liyang.cell(1,0).value )
 print(liyang.cell(1,1).value )
+
+import xlwt
+workbook = xlwt.Workbook()
+liyang = workbook.add_sheet('liyang')
+data = [
+  ['name', 'id'],
+  ['liyang', '1929'],
+  ['sam', '2111'],
+]
+
+for i in range(0, len(data)):
+  for j in range(0, len(data[i])):
+    liyang.write(i, j, data[i][j] )
+
+workbook.save('/root/sam.xls')
+
 
