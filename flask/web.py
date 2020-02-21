0df5f6c37f927c92858529c299cdd3cb919f51d8
liyang <235938019@qq.com>
2020-02-21 13:01:12 +0000

    liyang

--- flask/web.py
+++ flask/web.py
@@ -1,18 +1,25 @@
 from flask import Flask
 from flask import render_template
+import pymysql
 
 app = Flask(__name__)
 
 @app.route('/')
+
 def hello():
   name={'username': 'root'}
-  rows=[
-    {'name': 'liyang','phone': '18116583538','age': 40},
-    {'name': 'liyang','phone': '18116583538','age': 40},
-    {'name': 'liyang','phone': '18116583538','age': 40},
-    {'name': 'liyang','phone': '18116583538','age': 40},
-  ]
-  return render_template('index.html',name=name,rows=rows,title='sam')
+
+  connection=pymysql.connect("192.168.20.161","root","liyang","liyang")
+  cursor=connection.cursor(pymysql.cursors.DictCursor)
+
+  sql="select * from person;"
+  count=cursor.execute(sql)
+  result=cursor.fetchall()
+
+  cursor.close()
+  connection.close()
+
+  return render_template('index.html',name=name,rows=result,title='sam')
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0',port=80,debug=True)
