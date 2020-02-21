cedd0c9ca75abf004ffe92d4b350dce67e160d70
liyang <235938019@qq.com>
2020-02-21 12:27:18 +0000

    liyang

--- flask/web.py
+++ flask/web.py
@@ -6,7 +6,13 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
   name={'username': 'root'}
-  return render_template('index.html',name=name)
+  rows=[
+    {'name': 'liyang','phone': '18116583538','age': 40},
+    {'name': 'liyang','phone': '18116583538','age': 40},
+    {'name': 'liyang','phone': '18116583538','age': 40},
+    {'name': 'liyang','phone': '18116583538','age': 40},
+  ]
+  return render_template('index.html',name=name,rows=rows,title='sam')
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0',port=80,debug=True)
