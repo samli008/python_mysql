1e5892e6a0ecfcc5e5926b2308ed528b5c0cf0d4
liyang <235938019@qq.com>
2020-02-21 08:03:49 +0000

    liyang

---
+++ flask/flask.py
@@ -0,0 +1,12 @@
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+  name='Flask'
+  return render_template('index.html',name=name)
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0',port=80,debug=True)
