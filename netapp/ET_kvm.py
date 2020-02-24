61ffa2a08858ad9bff71c9c6ff3ef1a567eafb58
liyang <235938019@qq.com>
2020-02-24 17:31:57 +0000

    liyang

--- netapp/ET_kvm.py
+++ netapp/ET_kvm.py
@@ -4,11 +4,9 @@ tree=ET.parse('c04.xml')
 root=tree.getroot()
 
 for vcpu in root.iter('vcpu'):
-  vcpu.attrib['current']='1'
+  vcpu.attrib['current']='2'
+  vcpu.text='10'
   print(vcpu.attrib['current'])
-
-for vcpu in root.iter('vcpu'):
-  vcpu.text='20'
   print(vcpu.text)
 
 tree.write('c04.xml')
