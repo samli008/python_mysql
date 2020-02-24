81daa50a7b5b6903b2368ccfe516bc3f47d3d1e0
liyang <235938019@qq.com>
2020-02-24 17:44:04 +0000

    liyang

--- netapp/ET_kvm.py
+++ netapp/ET_kvm.py
@@ -4,9 +4,14 @@ tree=ET.parse('c04.xml')
 root=tree.getroot()
 
 for vcpu in root.iter('vcpu'):
-  vcpu.attrib['current']='2'
-  vcpu.text='10'
+  vcpu.attrib['current']='1'
+  vcpu.text='5'
   print(vcpu.attrib['current'])
   print(vcpu.text)
+
+for mem in root.iter('currentMemory'):
+  mem.text='2048000'
+for memory in root.iter('memory'):
+  memory.text='2048000'
 
 tree.write('c04.xml')
