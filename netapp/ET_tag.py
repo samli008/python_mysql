6c9cb3be7b2eda57bc10b488e498445cad90571b
liyang <235938019@qq.com>
2020-02-26 05:03:50 +0000

    liyang

---
+++ netapp/ET_tag.py
@@ -0,0 +1,24 @@
+import xml.etree.ElementTree as ET
+
+def walkData(node,level): 
+  global layer
+  count=len(node.tag)
+  print(node.tag.rjust(count+level))
+
+  #for sub_nodes
+  children = node.getchildren() 
+  if len(children) == 0: 
+    return
+
+  for child in children: 
+    walkData(child,level+2) 
+  return 
+ 
+def getXml(xml): 
+  level=0
+  root = ET.parse(xml).getroot() 
+  walkData(root,level) 
+  
+if __name__ == '__main__':
+  xml=input("pls input xml name: ")
+  getXml(xml)   
