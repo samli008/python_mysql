a9664ab07307456f74bd64aeaad5ed8bb46a82f9
liyang <235938019@qq.com>
2020-02-27 13:07:09 +0000

    liyang

--- netapp/netapp-ontap.py
+++ netapp/netapp-ontap.py
@@ -1,14 +1,20 @@
 # pip3 install netapp-ontap -i https://mirrors.aliyun.com/pypi/simple
 
+# volume create with python
 from netapp_ontap import config
-from netapp_ontap.host_connect import HostConnection
+from netapp_ontap.host_connection import HostConnection
 from netapp_ontap.resources import Volume
 
-config.CONNECTION = HostConnection('192.168.20.111','admin','netapp123')
+config.CONNECTION = HostConnection('192.168.20.111','admin','netapp123',verify=False)
 config.CONNECTION.verify = False
 
-volume1 = Volume(name='vol_liyang',
-		 svm={'name': 'svm1'},
-		 aggregates=[{'name': 'aggr1'}])
+vol1=input("input vol name: ")
+size1=int(input("input vol size[GB]: "))
+size1=int(size1*1024**3)
+
+volume1 = Volume(name=vol1,
+                 size=size1,
+                 svm={'name': 'svm1'},
+                 aggregates=[{'name': 'aggr1_zw01'}])
 
 volume1.post()
