9faa1959dd4b313000dc60747085d92b10935f6f
liyang <235938019@qq.com>
2020-03-04 07:58:16 +0000

    liyang

--- ansible/minio/make_bucket.py
+++ ansible/minio/make_bucket.py
@@ -4,6 +4,7 @@ cq = Minio('samli008.f3322.net:9009',
                     access_key='admin',
                     secret_key='12345678',
                     secure=False)
+
 print("{a:10}{b:15}".format(a='bucket',b='createTime'))
 
 buckets = cq.list_buckets()
