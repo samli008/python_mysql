52bf4706f2ef8ded7f1643201a652ef0221fcb5d
liyang <235938019@qq.com>
2020-03-06 16:57:09 +0000

    liyang

--- ansible/minio/crud.py
+++ ansible/minio/crud.py
@@ -3,24 +3,23 @@ import botocore
 from botocore.exceptions import ClientError
 import logging
 
-AK='C5PR8CK8FB16NGAR125W'
-SK='12345678'
+AK='L9JGMYNWV474ON35T8N7'
+SK='al73vUQ1dVSCJ8A8beusVlMQMjZC4oEri6yQkQsN'
 
 s3=boto3.resource('s3',aws_access_key_id=AK,
                    aws_secret_access_key=SK,
-                   endpoint_url='http://192.168.20.143:7480')
-
-choose=input("pls input your choose[list,new,del,obj,up,down,delete]: ")
-
-if choose == 'list':
+                   endpoint_url='http://192.168.20.182:8084')
+def list():
   for bucket in s3.buckets.all():
     print('bucket name: %s' % bucket.name)
-elif choose == 'new':
+
+def new():
   new=input("pls intput new bucket name: ")
   s3.create_bucket(Bucket=new)
   for bucket in s3.buckets.all():
     print('bucket name: %s' % bucket.name)
-elif choose == 'del':
+
+def del1(): 
   del1=input("pls input del bucket name: ")
   try:
     bucket= s3.Bucket(del1)
@@ -28,26 +27,47 @@ elif choose == 'del':
     for bucket in s3.buckets.all():
       print('bucket name: %s' % bucket.name)
   except botocore.exceptions.ClientError as e:
-    print('bucket is not empty')
-elif choose == 'obj':
+    print('bucket is not empty or not exist')
+
+def list_obj():
   bucket_name = input("bucket name: ")
   bucket = s3.Bucket(bucket_name)
   for obj in bucket.objects.all():
     print('obj name: %s' % obj.key)
-elif choose == 'up':
+
+def up():
   bucket_name = input("bucket name: ")
   file = input("file name: ")
   s3.Object(bucket_name,file).upload_file(file)
-elif choose == 'down':
+
+def down():
   bucket_name = input("bucket name: ")
   file = input("file name: ")
   s3.Object(bucket_name,file).download_file(file)
-elif choose == 'delete':
+
+def delete():
   try:
     bucket_name = input("bucket name: ")
     bucket= s3.Bucket(bucket_name)
     bucket.objects.filter().delete()
   except botocore.exceptions.ClientError as e:
     print('bucket is not exist')
+
+choose=input("pls input your choose[list,new,del,obj,up,down,delete]: ")
+
+if choose == 'list':
+  list()
+elif choose == 'new':
+  new()
+elif choose == 'del':
+  del1()
+elif choose == 'obj':
+  list_obj()
+elif choose == 'up':
+  up()
+elif choose == 'down':
+  down()
+elif choose == 'delete':
+  delete()
 else:
   print("your choose error!")
