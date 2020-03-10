2713ba6c0c57eb08c058878b041c9f7c2f3dac7e
liyang <235938019@qq.com>
2020-03-10 04:29:57 +0000

    liyang

--- ansible/openstack/03_config_docker.sh
+++ ansible/openstack/03_config_docker.sh
@@ -1,3 +1,5 @@
+sed -i 's|ExecStart=/usr/bin/dockerd|ExecStart=/usr/bin/dockerd --insecure-registry 192.168.20.143:4000|g' /usr/lib/systemd/system/docker.service
+
 cd /etc/systemd/system/
 mkdir docker.service.d
 cd docker.service.d
